f72a58a31b53d9467e9b61268695a0fb5b319b4a
성채은 <schn1115@kakao.com>
2023-06-05 23:04:19 +0900

    2023/06/05

---
+++ src/algorithm/ch01/ex01/home/SumGauss.java
@@ -0,0 +1,18 @@
+package algorithm.ch01.ex01.home;
+
+import java.util.Scanner;
+
+class SumGauss {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("1에서 n까지의 총합을 구합니다.");
+		System.out.print("n의 값 : ");
+		int n = stdIn.nextInt();
+
+		int sum = (n + 1) * (n / 2) + (n % 2 == 1 ? (n + 1) / 2 : 0);
+
+		System.out.println("1에서 " + n + "까지의 총합은 " + sum + "입니다.");
+	}
+}
