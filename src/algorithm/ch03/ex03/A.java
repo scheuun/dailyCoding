cb6ab1398cb4a00a62ddd81a321c81dc6cea94a2
성채은 <schn1115@kakao.com>
2023-06-25 23:57:46 +0900

    2023/06/25

---
+++ src/algorithm/ch03/ex03/A.java
@@ -0,0 +1,13 @@
+package algorithm.ch03.ex03;
+// Comparable 인터페이스 구현
+//public class A implements Comparable<A> {
+//    compareTo 메서드 구현
+//    public int compareTo(A c) {
+//
+//    }
+//
+//    equals 메서드 구현
+//    public boolean equals(Object c) {
+//
+//    }
+//}
---
+++ src/algorithm/ch03/ex03/X.java
@@ -0,0 +1,12 @@
+package algorithm.ch03.ex03;
+
+public class X {
+//    필드, 메서드 등
+//    public static final Comparable<T> COMPARABLE = new Comp();
+
+//    private static class Comp implements  Comparable<T> {
+//        public int compare(T d1, T d2) {
+//
+//        }
+//    }
+}
