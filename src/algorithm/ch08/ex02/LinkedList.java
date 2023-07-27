0863ff067932d4c47de1cdd3b66be72993b14e7f
성채은 <schn1115@kakao.com>
2023-07-27 23:51:06 +0900

    2023/07/27

--- src/algorithm/ch08/ex02/LinkedList.java
+++ src/algorithm/ch08/ex02/LinkedList.java
@@ -1,5 +1,7 @@
 package algorithm.ch08.ex02;
 
+import com.sun.deploy.panel.SpecialTreeListener;
+
 import java.util.Comparator;
 
 public class LinkedList<E> {
@@ -31,5 +33,95 @@ public class LinkedList<E> {
             ptr = ptr.next;
         }
         return null;
+    }
+
+    public void addFirst(E obj) {
+        Node<E> ptr = head;
+        head = crnt = new Node<E>(obj, ptr);
+    }
+
+    public void addLast(E obj) {
+        if (head == null)
+            addFirst(obj);
+        else {
+            Node<E> ptr = head;
+            while (ptr.next != null)
+                ptr = ptr.next;
+            ptr.next = crnt = new Node<E>(obj, null);
+        }
+    }
+
+    public void removeFirst() {
+        if (head != null)
+            head = crnt = head.next;
+    }
+
+    public void removeLast() {
+        if (head != null) {
+            if (head.next == null)
+                removeFirst();
+            else {
+                Node<E> ptr = head;
+                Node<E> pre = head;
+
+                while (ptr.next != null) {
+                    pre = ptr;
+                    ptr = ptr.next;
+                }
+
+                pre.next = null;
+                crnt = pre;
+            }
+        }
+    }
+
+    public void remove(Node p) {
+        if (head != null) {
+            if (p == head)
+                removeFirst();
+            else {
+                Node<E> ptr = head;
+
+                while (ptr.next != p) {
+                    ptr = ptr.next;
+                    if (ptr == null) return;
+                }
+                ptr.next = null;
+                crnt = ptr;
+            }
+        }
+    }
+
+    public void removeCurrentNode() {
+        remove(crnt);
+    }
+
+    public void clear() {
+        while (head != null)
+            removeFirst();
+        crnt = null;
+    }
+
+    public boolean next() {
+        if (crnt == null || crnt.next == null)
+            return false;
+        crnt = crnt.next;
+        return true;
+    }
+
+    public void printCurrentNode() {
+        if (crnt == null)
+            System.out.println("선택한 노드가 없습니다.");
+        else
+            System.out.println(crnt.data);
+    }
+
+    public void dump() {
+        Node<E> ptr = head;
+
+        while (ptr != null) {
+            System.out.println(ptr.data);
+            ptr = ptr.next;
+        }
     }
 }
