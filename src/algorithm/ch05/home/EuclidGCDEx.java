dd6986e0b94583530df34e15069a05118c365c54
성채은 <schn1115@kakao.com>
2023-08-02 23:25:47 +0900

    2023/08/02

---
+++ src/algorithm/ch05/home/EuclidGCDEx.java
@@ -0,0 +1,27 @@
+package algorithm.ch05.home;
+
+import java.util.Scanner;
+
+class EuclidGCDEx {
+
+	static int gcd(int x, int y) {
+		while (y != 0) {
+			int temp = y;
+			y = x % y;
+			x = temp;
+		}
+		return x;
+	}
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("두 정수의 최대 공약수를 구합니다.");
+
+		System.out.print("정수를 입력하세요 : ");   int x = stdIn.nextInt();
+		System.out.print("정수를 입력하세요 : ");   int y = stdIn.nextInt();
+
+		System.out.println("최대 공약수는 " + gcd(x, y) + "입니다.");
+	}
+}
+
