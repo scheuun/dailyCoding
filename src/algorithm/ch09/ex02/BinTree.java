76c998c5a8e2c85216b714e9794f226538fd51a9
성채은 <schn1115@kakao.com>
2023-08-08 23:47:43 +0900

    2023/08/08

---
+++ src/algorithm/ch09/ex02/BinTree.java
@@ -0,0 +1,43 @@
+package algorithm.ch09.ex02;
+
+import java.util.Comparator;
+
+public class BinTree<K, V> {
+//    private K key;
+//    private V data;
+//    private Node<K, V> left;
+//    private Node<K, V> right;
+//
+//    Node(K key, V data, Node<K, V> left, Node<K, V> right) {
+//        this.key = key;
+//        this.data = data;
+//        this.left = left;
+//        this.right = right;
+//    }
+//
+//    K getKey() {
+//        return key;
+//    }
+//
+//    V getValue() {
+//        return data;
+//    }
+//
+//    void print() {
+//        System.out.println(data);
+//    }
+//
+//    private Node<K, V> root;
+//    private Comparator<? super K> comparator = null;
+//
+//    public BinTree() {
+//        root =  null;
+//    }
+//
+//    public BinTree(Comparator<? super K> c) {
+//        this();
+//        comparator = c;
+//    }
+
+
+}
