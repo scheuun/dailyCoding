083a4580efee23ce4eca135a65a5949f36a30a73
성채은 <schn1115@kakao.com>
2023-08-09 17:43:30 +0900

    2023/08/09

--- src/algorithm/ch09/ex02/Object.java
+++ src/algorithm/ch09/ex02/Object.java
@@ -2,11 +2,11 @@ package algorithm.ch09.ex02;
 
 public class Object {
 
-    static {
-        registerNatives();
-    }
-
-    public final native Class<?> getClass();
+//    static {
+//        registerNatives();
+//    }
+//
+//    public final native Class<?> getClass();
 
     public native int hashCode();
 
@@ -20,28 +20,28 @@ public class Object {
         return getClass().getName() + "@" + Integer.toHexString(hashCode());
     }
 
-    public final native void notify();
-
-    public final native void notifyAll();
-
-    public final native void wait(long timeout) throws InterruptedException;
-
-    public final void wait(long timeout, int nanos) throws InterruptedException {
-        if (timeout < 0) {
-            throw new IllegalArgumentException("timeout value is negative");
-        }
-        if (nanos < 0 || nanos > 999999) {
-            throw new IllegalArgumentException("nanosecond timeout value out of range");
-        }
-        if (nanos >= 500000 || (nanos != 0 && timeout == 0)) {
-            timeout++;
-        }
-        wait(timeout);
-    }
-
-    public final void wait() throws InterruptedException {
-        wait(0);
-    }
+//    public final native void notify();
+//
+//    public final native void notifyAll();
+//
+//    public final native void wait(long timeout) throws InterruptedException;
+//
+//    public final void wait(long timeout, int nanos) throws InterruptedException {
+//        if (timeout < 0) {
+//            throw new IllegalArgumentException("timeout value is negative");
+//        }
+//        if (nanos < 0 || nanos > 999999) {
+//            throw new IllegalArgumentException("nanosecond timeout value out of range");
+//        }
+//        if (nanos >= 500000 || (nanos != 0 && timeout == 0)) {
+//            timeout++;
+//        }
+//        wait(timeout);
+//    }
+//
+//    public final void wait() throws InterruptedException {
+//        wait(0);
+//    }
 
     protected void finalize() throws Throwable { }
 }
