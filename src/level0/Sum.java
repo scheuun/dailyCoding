62c50aa601c0b442d9c577eec6fd195528d0e34a
성채은 <schn1115@kakao.com>
2023-11-12 23:06:07 +0900

    두 수의 합

---
+++ src/level0/Sum.java
@@ -0,0 +1,13 @@
+package level0;
+/*
+정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+num1	num2	result
+2	3	5
+100	2	102
+ */
+public class Sum {
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
