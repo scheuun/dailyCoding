d1158360ccf885d7bc5ad3ac103fbd3590ed2b4e
성채은 <schn1115@kakao.com>
2023-11-12 23:08:42 +0900

    두 수의 나눗셈

---
+++ src/level0/Division.java
@@ -0,0 +1,14 @@
+package level0;
+/*
+정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+
+num1	num2	result
+3	2	1500
+7	3	2333
+1	16	62
+ */
+public class Division {
+    public int solution(int num1, int num2) {
+        return 1000 * num1 / num2;
+    }
+}
