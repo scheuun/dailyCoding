b84eecea43b6adad953e78b3f0874b8cb773d56a
성채은 <schn1115@kakao.com>
2023-12-04 13:51:04 +0900

    중복된 숫자 개수

---
+++ src/level0/A_E/DupNum.java
@@ -0,0 +1,16 @@
+package level0.A_E;
+
+import java.util.Arrays;
+
+/*
+정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+
+array	n	result
+[1, 1, 2, 3, 4, 5]	1	2
+[0, 2, 3, 4]	1	0
+ */
+public class DupNum {
+    public int solution(int[] array, int n) {
+        return (int) Arrays.stream(array).filter(i -> i == n).count();
+    }
+}
