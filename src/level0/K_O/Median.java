008648f844c1f002bb460afe18060c16807121d6
성채은 <schn1115@kakao.com>
2023-12-09 23:04:46 +0900

    중앙값 구하기

---
+++ src/level0/K_O/Median.java
@@ -0,0 +1,18 @@
+package level0.K_O;
+
+import java.util.Arrays;
+
+/*
+중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다.
+예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
+
+array	result
+[1, 2, 7, 10, 11]	7
+[9, -1, 0]	0
+ */
+public class Median {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        return array[array.length / 2];
+    }
+}
